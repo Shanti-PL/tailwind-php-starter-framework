ef59ee3f16fa3ee10542c79935b508b23006a9c9
Junyi Fan <jfan@aiims.com.au>
2023-11-08 14:18:03 +1100

    first commit

---
+++ app/components/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My PHP Website</title>
+    <link rel="stylesheet" href="./dist/styles/main.css">
+</head>
+<body>
---
+++ app/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include __DIR__ . '/components/header.php';
+
+include __DIR__ . '/components/main.php';
+
+include __DIR__ . '/components/footer.php';
+
+?>
